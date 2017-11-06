035f627d32024a1c000a459bdea736a23c7a432f
gnzlbg <gonzalobg88@gmail.com>
2017-11-06 15:11:10 +0100

    [arm] run Aarch64 tests on QEMU-system

---
+++ ci/linux-aarch64.sh
@@ -0,0 +1,16 @@
+set -ex
+
+mkdir -m 777 /qemu
+cd /qemu
+
+curl -LO http://ftp.ru.debian.org/debian/dists/jessie/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/linux
+curl -LO http://ftp.ru.debian.org/debian/dists/jessie/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/initrd.gz
+
+mv linux kernel
+
+mkdir init
+cd init
+gunzip -c ../initrd.gz | cpio -id
+rm ../initrd.gz
+cp /usr/aarch64-linux-gnu/lib/libgcc_s.so.1 usr/lib/
+chmod a+w .
