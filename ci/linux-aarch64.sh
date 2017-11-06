5422947e51dfd8c155b388d156d27d17a1fc1601
gnzlbg <gonzalobg88@gmail.com>
2017-11-06 15:37:42 +0100

    fixup

--- ci/linux-aarch64.sh
+++ ci/linux-aarch64.sh
@@ -3,8 +3,8 @@ set -ex
 mkdir -m 777 /qemu
 cd /qemu
 
-curl -LO http://ftp.ru.debian.org/debian/dists/jessie/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/linux
-curl -LO http://ftp.ru.debian.org/debian/dists/jessie/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/initrd.gz
+curl -LO http://ftp.debian.org/debian/dists/testing/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/linux
+curl -LO http://ftp.debian.org/debian/dists/testing/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/initrd.gz
 
 mv linux kernel
 
