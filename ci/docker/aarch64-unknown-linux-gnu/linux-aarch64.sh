45c7582d32789c3afffa2421356ef8df37a80bee
gnzlbg <gonzalobg88@gmail.com>
2017-11-08 18:46:46 +0100

    fixup

--- ci/docker/aarch64-unknown-linux-gnu/linux-aarch64.sh
+++ ci/docker/aarch64-unknown-linux-gnu/linux-aarch64.sh
@@ -8,7 +8,7 @@ curl -LO http://ftp.debian.org/debian/dists/testing/main/installer-arm64/2017082
 curl -LO http://ftp.debian.org/debian/dists/testing/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/initrd.gz
 
 mv linux kernel
-mv initrd.debian initrd.gz
+#mv initrd.debian initrd.gz
 
 mkdir init
 cd init
