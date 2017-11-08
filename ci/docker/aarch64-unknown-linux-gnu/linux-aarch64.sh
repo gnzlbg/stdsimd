fcc35581bad96424f7b5eaec1705d8d1be9c1db8
gnzlbg <gonzalobg88@gmail.com>
2017-11-08 19:20:52 +0100

    fixup

--- ci/docker/aarch64-unknown-linux-gnu/linux-aarch64.sh
+++ ci/docker/aarch64-unknown-linux-gnu/linux-aarch64.sh
@@ -3,7 +3,7 @@ set -ex
 mkdir -m 777 /qemu
 cd /qemu
 
-#curl -LO https://github.com/qemu/qemu/raw/master/pc-bios/s390-ccw.img
+curl -LO https://github.com/qemu/qemu/raw/master/pc-bios/efi-virtio.rom
 curl -LO http://ftp.debian.org/debian/dists/testing/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/linux
 curl -LO http://ftp.debian.org/debian/dists/testing/main/installer-arm64/20170828/images/netboot/debian-installer/arm64/initrd.gz
 
@@ -12,9 +12,7 @@ mv linux kernel
 
 mkdir init
 cd init
-file ../initrd.gz
 gunzip -c ../initrd.gz | cpio -id
 rm ../initrd.gz
-ls /usr/
 cp /usr/aarch64-linux-gnu/lib/libgcc_s.so.1 usr/lib/
 chmod a+w .
